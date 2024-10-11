9ff7d647d89384473c960f40bd6612639ec91ad0
RivJams <riverw1104@gmail.com>
2024-10-11 09:42:21 -0500

    Added in bits of previous code

---
+++ main.cpp
@@ -0,0 +1,29 @@
+//
+// Created by riwall01 on 10/11/2024.
+//
+
+#include <iostream> // required for couts & cins
+#include <cstdlib> // allows for randomizer
+#include <ctime> // enables use of time function
+#include <string> // allows for strings to be used
+
+using namespace std; // sets standard namespace
+
+int main() {
+    // Starting the main function
+
+    //*********************************************************************
+
+    // Declares and initializes variables
+    string userName = "unknown"; // Declaring and initializing variables
+    int leftNum = 0;
+    int rightNum = 0;
+    int mType = 0;
+    char mSymbol = '?';
+    int correctAnswer = 0;
+    int userAnswer = 0;
+    int temp = 0;
+    char userYN = '?';
+
+    srand(time(0)); // Generates a unique seed so its random.
+}
